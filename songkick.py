be27187486a1a47bdec9afa832bd97e21d0c9f0b
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2018-11-18 20:24:03 -0700

    fixed typo

--- songkick.py
+++ songkick.py
@@ -1,6 +1,6 @@
 ## TODO: remove line from xls file when successful?
 
-loqfrom selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
